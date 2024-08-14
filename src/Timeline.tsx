5200f75b975cefc658dc31aff2de9ba6e1e3370b
anasnadkar45 <anasnadkar23@gmail.com>
2024-08-14 20:58:17 +0530

    Updating Codebase

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { createShapeId, TLShapeId, useEditor } from 'tldraw';
 
 interface sizeOfTimelineProps {
